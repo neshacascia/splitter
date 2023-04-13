6d5216bb7b62df34c760375d99fb619608853dfd
Nesha Mervin <nesha.mervin@gmail.com>
2023-04-13 06:37:16 -0400

    completed styling layout for mobile screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgBody: '#C5E4E7',
+        title: '#3D6666',
+        darkGreen: '#00474B',
+        lightGreen: '#F3F9FA',
+        grayGreen: '#7F9D9F',
+        blueGreen: '#26C2AE',
+        heading: '#5E7A7D',
+        icon: '#9EBBBD',
+        custom: '#547878',
+      },
+      fontFamily: {
+        body: ['Space Mono'],
+      },
+    },
   },
   plugins: [],
 };
